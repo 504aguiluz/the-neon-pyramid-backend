308060610178d1558b3690a1915552d08574e62b
nazdak504 <aguiluz.matt@gmail.com>
2021-12-13 23:56:22 -0600

    completed and tested update dup ordered_dishes route

--- resources/ordered_dishes.py
+++ resources/ordered_dishes.py
@@ -68,6 +68,8 @@ def create_ordered_dish(order_id, dish_id):
     print('here\'s the order to:', model_to_dict(current_order))
     print('here\'s the dish:', model_to_dict(current_dish))
 
+
+
     new_ordered_dish = models.OrderedDish.create(qtyOrdered = 1, order=current_order, dish=current_dish, customer=current_user)
 
     print(f'{new_ordered_dish.dish.title} added to order {current_order}')
@@ -85,6 +87,79 @@ def create_ordered_dish(order_id, dish_id):
         status = 200,
     ), 200
 
+
+# update dup ordered_dishes -> /api/v1/ordered_dishes/<order_id>/<dish_id>/ =====================================================
+@ordered_dishes.route('/<order_id>/<dish_id>/<ordered_dish_id>', methods=['PUT'])
+# @login_required
+def update_duplicate_ordered_dish(order_id, dish_id, ordered_dish_id):
+    current_order = models.Order.get_by_id(order_id)
+    current_dish = models.Dish.get_by_id(dish_id)
+    current_ordered_dish = models.OrderedDish.get_by_id(ordered_dish_id)
+
+
+    print('here\'s the current order id:', (current_order.id))
+    print('here\'s the dish id:', (current_dish.id))
+
+
+    # queries what dishes an order has:
+    dishes = (models.Dish
+            .select()
+            .join(models.OrderedDish)
+            .join(models.Order)
+            .where(models.Order.id == current_order.id))
+    
+    # queries ordered_dish containing current_order_id:
+    ordered_dishes = (models.OrderedDish
+                        .select(models.OrderedDish, models.Dish, models.Order)
+                        .join(models.Dish)
+                        .switch(models.OrderedDish)
+                        .join(models.Order)
+                        .where(models.Order.id == current_order.id))
+
+    # make list of dicts
+    dishes_dict = [model_to_dict(dish) for dish in dishes]
+    ordered_dishes_dict = [model_to_dict(ordered_dish) 
+    for ordered_dish in ordered_dishes]
+    current_ordered_dish_dict = model_to_dict(current_ordered_dish)
+
+    for od in ordered_dishes:
+        target = model_to_dict(od)
+        if target['dish']['id'] == current_dish.id:
+            print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+            print('target dish id:')
+            print(target['id'])
+            print('current ordered dish id:')
+            print(current_ordered_dish_dict['id'])
+            # announce duplicate
+            print(f'ordered dish with id: {target} is a duplicate!')
+            # increment current_ordered_dish__dict_qtyOrdered
+            current_ordered_dish_dict['qtyOrdered'] += 1
+            print('new current OD qtyOrdered:')
+            # double current_ordered_dish__dict_price
+            print(current_ordered_dish_dict['qtyOrdered'])
+            current_ordered_dish_dict['dish']['price'] *= 2
+            print('new current OD price:')
+            print(current_ordered_dish_dict['dish']['price'])
+            # delete target
+            delete_query = models.OrderedDish.delete().where(models.OrderedDish.id == target['id'])
+            num_of_rows_deleted = delete_query.execute()
+            print(f'num of rows deleted: {num_of_rows_deleted}')
+        else:
+            print('====================================')
+            print('target dish id:')
+            print(target['dish']['id'])
+            print('current ordered dish:')
+            print(current_ordered_dish_dict['id'])
+            print('not a duplicate')
+
+    return jsonify(
+        data = current_ordered_dish_dict,
+        status = {
+            "code": 200,
+            "message": 'ordered dish updated successfully!'
+        }
+    ), 200
+    
 
 # delete ordered_dish -> /api/v1/ordered_dishes/<order_id>/<dish_id>/ =====================================================
 @ordered_dishes.route('/<ordered_dish_id>/', methods=['DELETE'])
